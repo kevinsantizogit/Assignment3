659a0981b4a63c1cd28da6464b44da09cf721b67
Kevin <santizok@mcmaster.ca>
2026-03-15 12:22:04 -0400

    Forgot to mention articles.js updates, added functions

--- controllers/articles.js
+++ controllers/articles.js
@@ -5,7 +5,7 @@ const ArticlesModel = require('../models/articles.js')
 // Display the articles page
 router.get("/", async function(req, res)
 {
-  // Retrieve all of the articles using the model method, display the page
+  // Forgot to mention this in previous commit
   let results = await ArticlesModel.getAllArticles();
   req.TPL.articles = results;
   res.render("articles",
