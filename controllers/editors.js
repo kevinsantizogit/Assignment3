13feebe121b9c14bc438c34c061262ca54a583fd
Kevin <santizok@mcmaster.ca>
2026-03-15 12:00:18 -0400

    Load functionality for edtior controllerr

--- controllers/editors.js
+++ controllers/editors.js
@@ -1,10 +1,24 @@
 const express = require('express');
-var router = express.Router()
+var router = express.Router();
+const UsersModel = require('../models/users.js');
+const ArticlesModel = require('../models/articles.js');
 
-// Display the editors page
-router.get("/", async function(req, res)
-{
+// Display the editors page 
+router.get("/", async function(req, res) {
+  req.TPL.users = await UsersModel.getAllUsers();
+  req.TPL.articles = await ArticlesModel.getAllArticlesWithId();
   res.render("editors", req.TPL);
+});
+
+router.get("/deletearticle/:rowid", async function(req, res) {
+  await ArticlesModel.deleteArticle(req.params.rowid);
+  res.redirect("/editors");
+});
+
+router.get("/deleteuser/:username", async function(req, res) {
+  await ArticlesModel.deleteArticlesByUser(req.params.username);
+  await UsersModel.deleteUser(req.params.username);
+  res.redirect("/editors");
 });
 
 module.exports = router;
--- models/articles.js
+++ models/articles.js
@@ -8,25 +8,39 @@ async function init() {
       driver: sqlite3.Database
     });
   } catch(err) {
-      console.error(err);
+    console.error(err);
   }
 }
 
 init();
 
 // Return all of the articles
-async function getAllArticles()
-{
-  let results = await db.all("SELECT * FROM Articles");
-  return results;
+async function getAllArticles() {
+  return await db.all("SELECT * FROM Articles");
+}
+
+// Return all articles with their rowids (used on the editors page)
+async function getAllArticlesWithId() {
+  return await db.all("SELECT rowid, * FROM Articles");
 }
 
 // Create a new article given a title, content and username
-async function createArticle(article,username)
-{
+async function createArticle(article, username) {
   await db.run("INSERT INTO Articles VALUES (?,?,?)",
          [article.title, username, article.content]);
 }
 
-module.exports = {getAllArticles
-                 ,createArticle};
+async function deleteArticle(rowid) {
+  await db.run("DELETE FROM Articles WHERE rowid = ?", [rowid]);
+}
+
+
+async function deleteArticlesByUser(username) {
+  await db.run("DELETE FROM Articles WHERE username = ?", [username]);
+}
+
+module.exports = { getAllArticles
+                 , getAllArticlesWithId
+                 , createArticle
+                 , deleteArticle
+                 , deleteArticlesByUser };
